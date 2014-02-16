2dbfe09c34c7f30637febbd7c96cb8f534f9bb49
cziner21 <cziner.adam@gmail.com>
2014-02-16 12:27:04 +0100

    Alap vektoros feladat, szálak nélkül
    Készítsünk egy osztályt, mely tartalmaz egy 500 000 elemű egész vektort, ezt a vektort az osztályszintű konstruktorral töltsük fel egyesekkel, majd írjunk 2 visszatérési érték és paraméter nélküli metódust, az egyik az alsó másik a felső felét összegezze a tömbnek egy statikus sum változóba.

---
+++ Vektoros/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vektoros
+{
+    
+    class Tombos
+    {
+        int[] tomb = new int[500000];
+        public static int sum;
+        public Tombos() {
+            for (int i = 0; i < tomb.Length; i++) {
+                tomb[i] = 1;
+            }
+            sum = 0;
+        }
+
+        public void AlsoFele() {
+            for (int i = 0; i < tomb.Length / 2; i++) {
+                sum += tomb[i];
+            }            
+        }
+
+        public void FelsoFele() {
+            for (int i = tomb.Length / 2; i < tomb.Length; i++) {
+                sum += tomb[i];
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Tombos t = new Tombos();
+            t.AlsoFele();
+            t.FelsoFele();
+            Console.WriteLine("Az összeg: {0}", Tombos.sum);
+            Console.ReadKey();
+        }
+    }
+}
